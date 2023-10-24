2905362020dfd411197c945d54ff3e3d9ec3aa3c
Justdwiwt <len_master@hotmail.com>
2023-10-24 20:51:54 +0800

    leetCode 352

--- src/main/java/Solution_352.java
+++ src/main/java/Solution_352.java
@@ -1,33 +1,34 @@
 package main.java;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Solution_352 {
     static class SummaryRanges {
-        List<Integer> list;
+        int[] nums = new int[10001];
+        int max = 0;
 
         public SummaryRanges() {
-            list = new ArrayList<>();
         }
 
         public void addNum(int val) {
-            list.add(val);
+            if (nums[val] == 0) nums[val] = val + 1;
+            max = Math.max(max, val);
         }
 
         public int[][] getIntervals() {
-            if (list.size() == 1) return new int[][]{{list.get(0), list.get(0)}};
-            Collections.sort(list);
-            List<int[]> arr = new ArrayList<>();
-            int l = list.get(0);
-            for (int i = 1; i < list.size(); i++)
-                if (list.get(i) - list.get(i - 1) > 1) {
-                    arr.add(new int[]{l, list.get(i - 1)});
-                    l = list.get(i);
+            List<int[]> res = new ArrayList<>();
+            for (int i = 0; i <= max; i++)
+                if (nums[i] != 0) {
+                    int j = find(nums[i]) - 1;
+                    res.add(new int[]{i, j});
+                    i = j;
                 }
-            arr.add(new int[]{l, list.get(list.size() - 1)});
-            return arr.toArray(new int[arr.size()][]);
+            return res.toArray(new int[0][2]);
+        }
+
+        private int find(int x) {
+            return nums[x] == 0 ? x : (nums[x] = find(nums[x]));
         }
     }
 }
