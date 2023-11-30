e6481057c929081d7303af9d8a707010537ebc49
Justdwiwt <len_master@hotmail.com>
2023-11-30 21:34:46 +0800

    leetCode 299

---
+++ src/main/java/Solution_299.java
@@ -0,0 +1,15 @@
+package main.java;
+
+public class Solution_299 {
+    public String getHint(String secret, String guess) {
+        int[] nums = new int[10];
+        int countA = 0, countB = 0;
+        for (int i = 0; i < secret.length(); i++)
+            if (secret.charAt(i) == guess.charAt(i)) countA++;
+            else {
+                if (nums[guess.charAt(i) - '0']-- > 0) countB++;
+                if (nums[secret.charAt(i) - '0']++ < 0) countB++;
+            }
+        return countA + "A" + countB + "B";
+    }
+}
