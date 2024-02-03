a29db3b8918d8b0086ad51568943eebc34dcc0ed
Justdwiwt <len_master@hotmail.com>
2024-02-03 17:56:12 +0800

    leetCode 198

---
+++ src/main/java/Solution_198.java
@@ -0,0 +1,13 @@
+package main.java;
+
+public class Solution_198 {
+    public int rob(int[] nums) {
+        int pre = 0, cur = 0, tmp;
+        for (int num : nums) {
+            tmp = cur;
+            cur = Math.max(pre + num, cur);
+            pre = tmp;
+        }
+        return cur;
+    }
+}
