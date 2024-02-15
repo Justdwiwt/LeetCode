964df5d15c7ec2e0665347a7001297d77e7afd1a
Justdwiwt <len_master@hotmail.com>
2024-02-16 00:57:02 +0800

    leetCode 167

---
+++ src/main/java/Solution_167.java
@@ -0,0 +1,13 @@
+package main.java;
+
+public class Solution_167 {
+    public int[] twoSum(int[] numbers, int target) {
+        for (int i = 0, j = numbers.length - 1; i < j; ) {
+            int sum = numbers[i] + numbers[j];
+            if (sum == target) return new int[]{i + 1, j + 1};
+            else if (sum > target) j--;
+            else i++;
+        }
+        return null;
+    }
+}
