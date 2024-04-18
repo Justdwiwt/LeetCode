cacdd50fa6a9fdd868a5289c50f9490f15bac85e
Justdwiwt <len_master@hotmail.com>
2024-04-18 19:02:44 +0800

    leetCode 104

---
+++ src/main/java/Solution_104.java
@@ -0,0 +1,10 @@
+package main.java;
+
+public class Solution_104 {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+}
