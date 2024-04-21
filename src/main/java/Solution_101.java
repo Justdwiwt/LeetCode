7034ab2709d2e832832bc663c1a1f20215d3d472
Justdwiwt <len_master@hotmail.com>
2024-04-21 10:52:49 +0800

    leetCode 101

---
+++ src/main/java/Solution_101.java
@@ -0,0 +1,13 @@
+package main.java;
+
+public class Solution_101 {
+    public boolean isSymmetric(TreeNode root) {
+        return dfs(root.left, root.right);
+    }
+
+    public boolean dfs(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) return true;
+        if (root1 == null || root2 == null || root1.val != root2.val) return false;
+        return dfs(root1.left, root2.right) && dfs(root1.right, root2.left);
+    }
+}
