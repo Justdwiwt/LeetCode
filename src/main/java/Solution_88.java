f356f8935dd1b9874c1036de07947ee6781ce9aa
Justdwiwt <len_master@hotmail.com>
2024-05-04 11:14:55 +0800

    leetCode 88

---
+++ src/main/java/Solution_88.java
@@ -0,0 +1,10 @@
+package main.java;
+
+import java.util.Arrays;
+
+public class Solution_88 {
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        System.arraycopy(nums2, 0, nums1, m, n);
+        Arrays.sort(nums1);
+    }
+}
