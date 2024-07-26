50104651267be27509a5b6dd52a130f36da53f02
Justdwiwt <len_master@hotmail.com>
2024-07-26 09:33:50 +0800

    leetCode 25

---
+++ src/main/java/Solution_25.java
@@ -0,0 +1,24 @@
+package main.java;
+
+public class Solution_25 {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(0), prev = dummy, curr = head, next;
+        dummy.next = head;
+        int length = 0;
+        while (head != null) {
+            length++;
+            head = head.next;
+        }
+        for (int i = 0; i < length / k; i++) {
+            for (int j = 0; j < k - 1; j++) {
+                next = curr.next;
+                curr.next = next.next;
+                next.next = prev.next;
+                prev.next = next;
+            }
+            prev = curr;
+            curr = prev.next;
+        }
+        return dummy.next;
+    }
+}
