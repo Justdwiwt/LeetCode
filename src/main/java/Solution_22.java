eb8ebbf82fe99f06e03956d33e438f65a498f03d
Justdwiwt <len_master@hotmail.com>
2024-07-29 09:29:23 +0800

    leetCode 22

---
+++ src/main/java/Solution_22.java
@@ -0,0 +1,22 @@
+package main.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution_22 {
+    List<String> res = new ArrayList<>();
+
+    public List<String> generateParenthesis(int n) {
+        dfs(n, n, "");
+        return res;
+    }
+
+    private void dfs(int left, int right, String curStr) {
+        if (left == 0 && right == 0) {
+            res.add(curStr);
+            return;
+        }
+        if (left > 0) dfs(left - 1, right, curStr + "(");
+        if (right > left) dfs(left, right - 1, curStr + ")");
+    }
+}
