8a6910f7d0011c750d053144ca6068a37f992b26
Justdwiwt <len_master@hotmail.com>
2024-09-14 10:07:11 +0800

    leetCode 2323

--- src/main/java/Solution_2323.java
+++ src/main/java/Solution_2323.java
@@ -1,15 +1,17 @@
 package main.java;
 
-import java.util.stream.IntStream;
+import java.util.Arrays;
 
 public class Solution_2323 {
-    public String decodeMessage(String key, String message) {
-        char[] map = new char[26];
-        char curChar = 'a';
-        for (char i : key.toCharArray())
-            if (i != ' ' && map[i - 'a'] == 0) map[i - 'a'] = curChar++;
-        char[] res = message.toCharArray();
-        IntStream.range(0, res.length).filter(i -> res[i] != ' ').forEach(i -> res[i] = map[res[i] - 'a']);
-        return new String(res);
+    public int minimumTime(int[] jobs, int[] workers) {
+        int minTime = 0;
+        Arrays.sort(jobs);
+        Arrays.sort(workers);
+        int n = jobs.length;
+        for (int i = 0; i < n; i++) {
+            int currTime = (jobs[i] - 1) / workers[i] + 1;
+            minTime = Math.max(minTime, currTime);
+        }
+        return minTime;
     }
 }
