dcade43f2556fb6800de61a6ac8662f0ea25df11
Justdwiwt <len_master@hotmail.com>
2024-12-14 21:56:20 +0800

    leetCode 2433

---
+++ src/main/java/Solution_2433.java
@@ -0,0 +1,8 @@
+package main.java;
+
+public class Solution_2433 {
+    public int[] findArray(int[] pref) {
+        for (int i = pref.length - 1; i > 0; i--) pref[i] ^= pref[i - 1];
+        return pref;
+    }
+}
