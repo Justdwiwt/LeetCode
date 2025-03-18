8eb7b6fbe1ab823fc12f2936da34d2e924f432bb
Justdwiwt <len_master@hotmail.com>
2025-03-18 22:50:07 +0800

    leetCode 2540

---
+++ src/main/java/Solution_2540.java
@@ -0,0 +1,12 @@
+package main.java;
+
+public class Solution_2540 {
+    public int getCommon(int[] nums1, int[] nums2) {
+        int left = 0, right = 0;
+        while (left < nums1.length && right < nums2.length)
+            if (nums1[left] == nums2[right]) return nums1[left];
+            else if (nums1[left] < nums2[right]) left++;
+            else right++;
+        return -1;
+    }
+}
