6894a2d4feefa7f45fd9e3fb6e9e6b1d11fb1b6c
Justdwiwt <len_master@hotmail.com>
2025-05-08 22:16:24 +0800

    leetCode 2394

---
+++ src/main/java/Solution_2394.java
@@ -0,0 +1,21 @@
+package main.java;
+
+import java.util.Arrays;
+
+public class Solution_2394 {
+    public long repairCars(int[] ranks, int cars) {
+        long left = 1;
+        long right = (long) 1e14;
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            if (isBlue(ranks, cars, mid)) right = mid - 1;
+            else left = mid + 1;
+        }
+        return left;
+    }
+
+    boolean isBlue(int[] ranks, int cars, long mid) {
+        long res = Arrays.stream(ranks).mapToLong(a -> (int) Math.sqrt((double) mid / a)).sum();
+        return res >= cars;
+    }
+}
