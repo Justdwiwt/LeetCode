fb88afb003b3ef3e5e6ef58f34bcce901379813c
Justdwiwt <len_master@hotmail.com>
2025-06-06 20:00:53 +0800

    leetCode 2651

---
+++ src/main/java/Solution_2651.java
@@ -0,0 +1,7 @@
+package main.java;
+
+public class Solution_2651 {
+    public int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+        return (arrivalTime + delayedTime) % 24;
+    }
+}
