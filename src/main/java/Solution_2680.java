95b519936ef2b630fee5a45c50ab8458bbf08e08
Justdwiwt <len_master@hotmail.com>
2025-06-24 22:25:10 +0800

    leetCode 2680

---
+++ src/main/java/Solution_2680.java
@@ -0,0 +1,18 @@
+package main.java;
+
+public class Solution_2680 {
+    public long maximumOr(int[] nums, int k) {
+        int n = nums.length;
+        int[] suf = new int[n];
+        for (int i = n - 2; i >= 0; i--)
+            suf[i] = suf[i + 1] | nums[i + 1];
+
+        long ans = 0;
+        int pre = 0;
+        for (int i = 0; i < n; i++) {
+            ans = Math.max(ans, pre | ((long) nums[i] << k) | suf[i]);
+            pre |= nums[i];
+        }
+        return ans;
+    }
+}
