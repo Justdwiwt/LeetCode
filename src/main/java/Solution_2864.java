61bf204c0e7e766978a4ebf2a64f5e146dc85a56
Justdwiwt <len_master@hotmail.com>
2025-10-19 19:59:14 +0800

    leetCode 2864

---
+++ src/main/java/Solution_2864.java
@@ -0,0 +1,9 @@
+package main.java;
+
+public class Solution_2864 {
+    public String maximumOddBinaryNumber(String s) {
+        String replace = s.replace("0", "");
+        String replace1 = s.replace("1", "");
+        return replace.replaceFirst("1", "") + replace1 + "1";
+    }
+}
